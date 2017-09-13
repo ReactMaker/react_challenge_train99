5a22991fd3ddd16ef59904740280b0e1a89a1806
chnbohwr <chnbohwr@gmail.com>
2017-09-13 21:59:41 +0800

    make key event

--- src/containers/Home/components/Content.js
+++ src/containers/Home/components/Content.js
@@ -4,28 +4,53 @@ import Bullet from './Bullet';
 
 import './Content.less';
 
-const keyMap = {
-  left: 37,
-  up: 38,
-  right: 39,
-  down: 40,
-};
-
 export default class Content extends Component {
   state = {
     time: 0,
     planeX: 250,
     planeY: 250,
+    gameStatus: false,
   }
-  onKeyDown = (e) => {
+  keyPressed = {}
+
+  movePlane = () => {
     const keyCase = {
       37: () => { this.setState({ planeX: this.state.planeX - 1 }); },
       38: () => { this.setState({ planeY: this.state.planeY - 1 }); },
       39: () => { this.setState({ planeX: this.state.planeX + 1 }); },
       40: () => { this.setState({ planeY: this.state.planeY + 1 }); },
     };
-    keyCase[e.keyCode]();
+    Object.keys(this.keyPressed).map(key => keyCase[key]());
   }
+
+  registKeyDown = (e) => {
+    this.keyPressed[e.keyCode] = true;
+  }
+
+  registKeyUp = (e) => {
+    delete this.keyPressed[e.keyCode];
+  }
+
+  gameLoop = () => {
+    this.movePlane();
+    if (this.state.gameStatus) {
+      window.requestAnimationFrame(this.gameLoop);
+    }
+  }
+
+  toggleGame = () => {
+    if (!this.state.gameStatus) {
+      this.setState({ gameStatus: true }, this.gameLoop);
+      window.document.addEventListener('keydown', this.registKeyDown, false);
+      window.document.addEventListener('keyup', this.registKeyUp, false);
+    } else {
+      this.setState({ gameStatus: false });
+      window.document.removeEventListener('keydown', this.registKeyDown);
+      window.document.removeEventListener('keyup', this.registKeyUp);
+    }
+
+  }
+
   render() {
     const { time, planeX, planeY } = this.state;
     const planeStyle = {
@@ -34,7 +59,8 @@ export default class Content extends Component {
     };
     return (
       <div className="content">
-        <div id="stage" tabIndex="0" onKeyDown={this.onKeyDown}>
+        <button onClick={this.toggleGame}> toggle game {String(this.state.gameStatus)}</button>
+        <div id="stage">
           <Bullet x={100} y={100} />
           <div id="gameTime">now time: {time}</div>
           <img id="master" src={planeImg} alt="airplane" style={planeStyle} />
